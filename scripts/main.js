fd9076d87f112c906958b76ff200dd70731f3fad
jakecurrie <jake@findandseacreative.com>
2019-06-24 14:38:20 +0800

    some changes haha

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+// ADD JS COMMENTS INSIDE THIS SECTION
+
+    $(".hamburger").click(function(){
+
+    $('.hamburger').toggleClass('active');
+    $(".mobile-nav").slideToggle();
+    });
+
+}); // end document ready
