4ce7a0a78c8a850b572a8bb6f89a7f06cd8ced67
jakecurrie <jake@findandseacreative.com>
2020-03-17 10:43:39 +1000

    brand new website

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,45 @@
-$(document).ready(function(){
-// ADD JS COMMENTS INSIDE THIS SECTION
+$ (document).ready(function(){
+  $(".hamburger").click(function(){
 
-    $(".hamburger").click(function(){
+       $('.hamburger').toggleClass('active');
+       $(".mobile-nav").slideToggle();
+     });
 
-    $('.hamburger').toggleClass('active');
-    $(".mobile-nav").slideToggle();
-    });
+// This is for the animations
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
 
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 0, // offset (in px) from the original trigger point
+  delay: 50, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
-// Transition scroll bar
-
+});
 $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+var scroll = $(window).scrollTop();
 // 10 is the value of pixels in order to active this jq
-    if (scroll <= 10) {
-        $("header").removeClass("whitebg");
-    } else {
-        $("header").addClass("whitebg");
-    }
+if (scroll <= 1) {
+    $("header").removeClass("whitebg" );
+} else {
+    $("header").addClass("whitebg");
+}
 });
 
-}); // end document ready
+});
